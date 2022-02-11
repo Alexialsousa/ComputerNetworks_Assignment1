fc2e296b1cf76b00c348ffaa16b568a23516513a
Alexia Sousa <alexialsousa@hotmail.com>
2022-02-10 23:28:15 -0500

    get request works

--- http_requests.py
+++ http_requests.py
@@ -2,21 +2,21 @@ import sys, socket
 
 def get(url, verbose):
     # url= "www.httpbin.org/get?course=networking&assignment=1%27"
-    url= "www.httpbin.org"
-    # url= "www.google.ca"
+    host = "www.httpbin.org"
+    args = "/get?course=networking&assignment=1%27"
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((url, 80))
-    message  = b'GET / HTTP/1.1\r\n'
-    message += str.encode('Host: ' + url + ':80\r\n')
+    s.connect((host, 80))
+    message  = str.encode('GET ' + args + ' HTTP/1.1\r\n')
+    message += str.encode('Host: ' + host + ':80\r\n')
     message += b'Connection: close\r\n'
     message += b'\r\n'
     s.sendall(message)
     result = s.recv(10000)
     result = result.decode("utf-8")
+
+    headers = result.split("\r\n\r\n")[0]
+    body = result.split("\r\n\r\n")[1]
     if verbose:
-        print("\n")
-        print(result)
-    # while (len(result) > 0):
-    #     print(result)
-    #     result = s.recv(10000)
+        print(headers + "\n")
+    print(body)
     s.close()
